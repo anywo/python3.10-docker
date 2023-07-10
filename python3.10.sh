6b93132366a4346bd7a81db58a3d90d48aff6365
anywo <1377593732@qq.com>
2023-07-10 22:26:15 +0800

    upd: 修改为默认相对路径

--- python3.10.sh
+++ python3.10.sh
@@ -1,1 +1,10 @@
-docker compose -f "${HOME}/workspace/python/3.10/docker-compose.yaml" run --rm py -c "python3 $*"
+# 获取当前工作目录
+current_dir="$PWD"
+
+# 获取用户主目录
+home_dir="$HOME"
+
+# 将$HOME替换为/root/u
+new_dir="${current_dir//$home_dir/\/root\/u}"
+
+docker compose -f "${HOME}/workspace/python/3.10/docker-compose.yaml" run --rm py -c "cd $new_dir && python3 $*"
